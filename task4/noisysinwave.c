9fbe8813ab12f9ab1fb60e31e2348ca059887222
James Hoyland <hoyland.jd@gmail.com>
2019-10-21 09:52:00 -0700

    James commets on task5

---
+++ task4/noisysinwave.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int main()
+{
+	FILE * input_file; //makes it into a file 
+
+    float x=0;
+    float y=0;
+    int i =0;
+
+	input_file = fopen("noisysinwave","w");	//names the file "noisysinwave"
+
+
+	srand(time(NULL));		//gives me random numbers based on system time 
+	//int r = rand();
+
+
+	//float r = rand()%10;	//varies r into 10%
+
+
+ for(i=0; i<360; ++i)			//angles to 360 deg
+    {
+
+    	x = i*(M_PI/180);   	//calc to radians
+    	y = sin(x);				//the sine of that radian
+
+    	float r = (float)rand()/(float)RAND_MAX;
+
+    	y+=(r/10);
+
+    	printf("%f\n",y);		
+    	fprintf(input_file,"\n%f", y);		//prints the x and y to the words deoc 
+	    
+	}
+
+	fclose(input_file);		//closes the file 
+
+
+	return 0;
+}
